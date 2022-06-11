a8edd28fa4663c1a6e9f58d9b39ce4c57bba8a19
VanessaRady <105666739+VanessaRady@users.noreply.github.com>
2022-06-11 00:25:54 -0500

    Actualicé pelicula.cpp

--- Reto/Avance1/Pelicula.cpp
+++ Reto/Avance1/Pelicula.cpp
@@ -1,10 +1,10 @@
 #include "Pelicula.h"
 
- Pelicula::Pelicula(){
+ Pelicula::Pelicula():Video(){
      oscares = 2022;
  }
 
- Pelicula::Pelicula(int _oscares){
+ Pelicula::Pelicula(string _iD, string _titulo, int _duracion, string _genero, double _calificacionPromedio, int _oscares): Video(_iD, _titulo, _duracion, _genero, _calificacionPromedio){
     oscares = _oscares;
 }
 
@@ -17,6 +17,12 @@
     int Pelicula::getOscares(){
         return oscares;
     }
+
+    ostream & operator<<(ostream &out, const Pelicula &v) {
+        out << v.iD << "," << v.titulo << "," << v.duracion << "," << v.genero << "," << v.calificacionPromedio << "," << v.oscares << endl;
+        return out;
+    }   
+
 
     // OTROS METODOS
     string Pelicula::str(){
