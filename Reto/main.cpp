6c2520c67f738b2e9522916e44fefd3ace105be0
VanessaRady <105666739+VanessaRady@users.noreply.github.com>
2022-06-15 12:57:22 -0500

    Saqué de carpeta

--- Reto/main.cpp
+++ Reto/main.cpp
@@ -1,37 +1,47 @@
 #include <iostream>
-#include "Video.h"
-#include "Pelicula.h"
-#include "Serie.h"
-#include "Episodio.h"
 #include "Peliculas.h"
-
+#include "Series.h"
 
 int menuPeliculas(){
     int iOpcion;
     
     cout <<
-    "\n ** Peliculas " <<
+    "\n *******  Peliculas *******" <<
     "\n1. Leer Peliculas  desde Archivo" <<
     "\n2. Reporte de todas las peliculas" <<
     "\n3. Reporte de peliculas que tienen cierta Calificacion" <<
     "\n4. Reporte de peliculas de cierto genero" <<
-    "\n0. SALIR\nTeclea la opcion:";
+    "\n\n *******  Series *******" <<
+    "\n5. Leer Series  desde Archivo" <<
+    "\n6. Reporte de todas las Series" <<
+    "\n7. Reporte de Series que tienen cierta Calificacion" <<
+    "\n8. Reporte de Series de cierto genero" <<
+    "\n9. Calcular calificación de todas las Series" <<
+    
+    "\n\n0. SALIR\nTeclea la opcion:"; 
     cin >> iOpcion;
     return iOpcion;
 }
-  
+// ******************************
+//            AVANCE 3
+// *******************************
 int main() {
     // se manda llamar al constructor - default
     Peliculas peliculas{};
+    // Polimorfismo - {} llama al constructor default Video
+   // Videos2 videos{};
+    Series series{};
+    //   Pelicula arrPeliculas[50], pelicula{};
     
     int iOpcion;
     string sGenero, sId;
     double dCal;
-    
+ 
     // Leer el archivo de peliculas y cargarlo en el arreglo de apuntadores
     // dentro de la clase Peliculas
     peliculas.leerArchivo();
-  
+    series.leerArchivo();
+    
     // 1o Inicializar la vcc antes del ciclo
     iOpcion = menuPeliculas();
     // 2a Incluir en la condicion la vcc
@@ -54,8 +64,28 @@ int main() {
                 cin >> sGenero;
                 peliculas.reporteGenero(sGenero);
                 break;
+            case 5: // leer videos
+               series.leerArchivo();
+                break;
+            case 6: // 6. Reporte de todas las Series"
+                series.reporteTodasLasSeries();
+                 break;
+            case 7:  // 7. Reporte de Series que tienen cierta Calificacion
+                cout << "Ingresa la calificación:";
+                cin >> dCal;
+                series.reporteConCalificacion(dCal);
+                 break;
+            case 8:  // 8. Reporte de Series de cierto genero
+                cout << "Ingresa el Genero:";
+                cin >> sGenero;
+                series.reporteGenero(sGenero);
+              //  videos.reporteVideos();
+                 break;
+            case 9:  // 9. Calcular calificación de todas las Series
+                series.calcularCalificacionSeries();
+                 break;
             default:
-                cout << "Opcion Incorrecta!!\n";
+                cout << "Opcion Incorrecta\n";
                 break;
         }
         //3o Actualizar la vcc dentro del ciclo
