4c59bb5571a757659a3324d2acecab4425f04176
VanessaRady <105666739+VanessaRady@users.noreply.github.com>
2022-06-15 13:12:40 -0500

    Update Serie.cpp

--- Reto/Serie.cpp
+++ Reto/Serie.cpp
@@ -3,8 +3,8 @@
 Serie::Serie():Video(){
   cantidad = 0;
 }
-Serie::Serie(string _iD, string _titulo, int _duracion, string _genero, double _calificacionPromedio):Video(_iD, _titulo, _duracion, _genero, 0){
-  cantidad = 0;
+Serie::Serie(string _iD, string _titulo, int _duracion, string _genero, double _calificacionPromedio, int _cantidad):Video(_iD, _titulo, _duracion, _genero, _calificacionPromedio){
+  cantidad = _cantidad;
 }
 
 void Serie::setEpisodio(int _index, Episodio _episodio){
@@ -28,21 +28,38 @@ int Serie::getCantidad(){
   return cantidad;
 }
 
-double Serie::calculaCalPromedio(){
-  double acum = 0;
-  for (int index = 0; index < cantidad; index++)
-    acum = acum + episodios[index].getCalificacion();
-
-  if (cantidad > 0)
-    return acum / cantidad;
-  else
-    return 0;
+double Serie::calculaCalPromedio()
+{
+    double acum = 0.0;
+    if (cantidad > 0)
+    {
+        for (int iR = 0; iR < cantidad; iR++) {
+            acum = acum + episodios[iR].getCalificacion();
+        }
+        calificacionPromedio = acum / cantidad;
+        return calificacionPromedio;
+    }
+    else
+    {
+        calificacionPromedio = -1;
+        return calificacionPromedio;
+    }
 }
+
+
+// Sobrecarga de operador <<
+ostream& operator<< (ostream &out, const Serie &s) {
+  out << s.iD << "," << s.titulo << "," << s.duracion << "," << s.genero << ", "<< s.calificacionPromedio << "," << s.cantidad << endl;
+
+  return out;
+
+}
+
 string Serie::str(){
-  // Concatenar todos los episodios de la serie
+    // Concatenar todos los episodios de la serie
   string acum = "\n";
   for (int index = 0; index < cantidad; index++)
-    acum = acum + episodios[index].str() + '\n';
-      
-  return iD + "," + titulo + ", " + to_string(duracion) + "," + genero + ", " + to_string(calificacionPromedio) + acum;
-}
+    acum = acum + episodios[index].str() + '\n'  ;
+    return iD + "," + titulo + ", " + to_string(duracion) + "," + genero + ", " + to_string(calificacionPromedio) + acum;
+    }
+
